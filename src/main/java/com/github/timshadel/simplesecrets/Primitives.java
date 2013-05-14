59c548c4e95c82233b6c3307aa54b33c7efe9399
Jay Wagnon <jay.wagnon@gmail.com>
2013-05-14 14:22:26 -0600

    Added derive_sender_hmac method.

--- src/main/java/com/github/timshadel/simplesecrets/Primitives.java
+++ src/main/java/com/github/timshadel/simplesecrets/Primitives.java
@@ -1,5 +1,7 @@
 package com.github.timshadel.simplesecrets;
 
+import java.security.GeneralSecurityException;
+import java.security.MessageDigest;
 import java.security.SecureRandom;
 
 public class Primitives
@@ -17,5 +19,31 @@ public class Primitives
     byte[] bytes = new byte[16];
     new SecureRandom().nextBytes(bytes);
     return bytes;
+  }
+
+
+  public static byte[] derive_sender_hmac(final byte[] master_key)
+          throws GeneralSecurityException
+  {
+    return derive(master_key, "simple-crypto/sender-hmac-key");
+  }
+
+
+  private static byte[] derive(final byte[] master_key, final String role)
+          throws GeneralSecurityException
+  {
+    assertBinarySize(master_key, 32);
+
+    MessageDigest digest = MessageDigest.getInstance("SHA-256");
+    digest.update(master_key);
+    digest.update(role.getBytes());
+    return digest.digest();
+  }
+
+
+  private static void assertBinarySize(final byte[] binary, int bytes)
+  {
+    if(binary == null || binary.length != bytes)
+      throw new IllegalArgumentException((bytes * 8) + "-bit byte array required.");
   }
 }
--- src/test/java/com/github/timshadel/simplesecrets/PrimitivesTest.java
+++ src/test/java/com/github/timshadel/simplesecrets/PrimitivesTest.java
@@ -1,11 +1,14 @@
 package com.github.timshadel.simplesecrets;
 
+
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
 import org.junit.Test;
 
+import java.security.GeneralSecurityException;
 import java.util.Arrays;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.*;
 
 public class PrimitivesTest
 {
@@ -17,5 +20,57 @@ public class PrimitivesTest
 
     byte[] another_nonce = Primitives.nonce();
     assertFalse("Nonce is not repeated", Arrays.equals(nonce, another_nonce));
+  }
+
+  @Test( expected = IllegalArgumentException.class)
+  public void test_derive_sender_hmac_null_master_key()
+          throws GeneralSecurityException
+  {
+    Primitives.derive_sender_hmac(null);
+  }
+
+  @Test( expected = IllegalArgumentException.class)
+  public void test_derive_sender_hmac_too_short_master_key()
+          throws GeneralSecurityException
+  {
+    Primitives.derive_sender_hmac(hexStringToBytes("bc", 31));
+  }
+
+  @Test( expected = IllegalArgumentException.class)
+  public void test_derive_sender_hmac_too_long_master_key()
+          throws GeneralSecurityException
+  {
+    Primitives.derive_sender_hmac(hexStringToBytes("bc", 33));
+  }
+
+  @Test
+  public void test_derive_sender_hmac()
+    throws GeneralSecurityException
+  {
+    byte[] master_key = hexStringToBytes("bc", 32);
+    byte[] expected = hexStringToBytes("1e2e2725f135463f05c268ffd1c1687dbc9dd7da65405697471052236b3b3088");
+
+    byte[] result = Primitives.derive_sender_hmac(master_key);
+    assertTrue("Derives sender HMAC key", Arrays.equals(expected, result));
+  }
+
+
+  private static final byte[] hexStringToBytes(String string)
+  {
+    return hexStringToBytes(string, 1);
+  }
+
+
+  private static final byte[] hexStringToBytes(String string, int repeat)
+  {
+    StringBuilder builder = new StringBuilder();
+    for(int i = 0; i < repeat; i++)
+      builder.append(string);
+    try {
+      return new Hex().decode(builder.toString().getBytes());
+    } catch (DecoderException e) {
+      e.printStackTrace();
+      return null;
+    }
   }
 }
